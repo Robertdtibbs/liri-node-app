bd0d56728a3ed7470c4d5260d2237887ebc954d8
Robert Tibbs <tibbsr09@gmail.com>
2019-04-15 21:02:57 -0400

    added better read.me and screenshots

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
-LIRI is Language Interpretation and Recognition Interface.  LIRI can be utilized to request information on concerts, movies, and songs.
+**LIRI** is Language Interpretation and Recognition Interface.  LIRI can be utilized to request information on concerts, movies, and songs.
 
-LIRI's three main functions are:
+**LIRI's** three main functions are:
 
- > concert-this -> then insert artist
- > spotify-this-song -> then insert song
- > movie-this -> then insert a movie title
+ > 1. concert-this -> then insert artist 
 
-Liri will then pull data specific to your request.  You can learn about upcoming concerts from your favorite artist, details about your new favorite song, and more information than you could ever want from that movie you just saw.
+![concert-this](screenshots/concert-this.JPG)
+
+ > 1. spotify-this-song -> then insert song
+
+![spotify-this-song](screenshots\spotify-this-song.JPG)
+
+ > 1. movie-this -> then insert a movie title
+
+![movie.this](screenshots/movie-this.JPG)
+
+**Liri** will then pull data specific to your request.  You can learn about upcoming concerts from your favorite artist, details about your new favorite song, and more information than you could ever want from that movie you just saw.
+
+
+
 
--- liri.js
+++ liri.js
@@ -16,68 +16,87 @@ var input = process.argv.slice(3);
 var type = input.join(' ')
 console.log(type);
 
-switch (selection){
-    
+//// switch here
+
+switch (selection) {
+
     case "concert-this":
-        var band = "https://rest.bandsintown.com/artists/" + type + "/events?app_id=codingbootcamp";
-        console.log(band)
-        axios.get(band).then(function(response){
-                // console.log(response.data);
-                console.log(response.data[0].venue.name);
-                console.log(response.data[0].venue.city + ", " + response.data[0].venue.region);
-                var concertDate = moment(response.data[0].datetime).format("MM/DD/YYYY hh:00 A")
-                console.log(concertDate);
-            })
+        concert();
         break;
     case "spotify-this-song":
-        if(!type){
-            type = "The Sign";
-        }
-
-        spotify.search({
-            type: "track",
-            query: type
-        }, function(err, data){
-            if(err){
-                console.log(err);
-            }
-            else{
-                // console.log(data);
-                console.log(data.tracks.items[0].artists[0].name)
-                console.log(data.tracks.items[0].name)
-                console.log(data.tracks.items[0].album.name)
-                console.log(data.tracks.items[0].external_urls.spotify)
-            }
-        })
+        songthis();
         break;
     case "movie-this":
-        if(!type){
-            type = "Mr Nobody";
-        }
-        
-        var movie = "http://www.omdbapi.com/?apikey=trilogy&t=" + type;
-        console.log(movie)
-        axios.get(movie).then(function(response){
-            console.log(response.data.Title);
-            console.log(response.data.Year);
-            console.log(response.data.Ratings[0].Value);
-            console.log(response.data.Ratings[1].Value);
-            console.log(response.data.Country);
-            console.log(response.data.Language);
-            console.log(response.data.Plot);
-            console.log(response.data.Actors);
-            })
+        moviethis();
         break;
     case "do-what-it-says":
-        console.log("Do what it do!")
-        fs.readFile("random.txt", "UTF8", function(err, data){
-            if(err){
-                console.log(err)
-            }else{
-                console.log(data)
-            }
-        })
+        whatitsay();
         break;
     default:
         console.log("Not the right question")
+}
+
+///// all function below this line.
+
+function concert() {
+    var band = "https://rest.bandsintown.com/artists/" + type + "/events?app_id=codingbootcamp";
+    axios.get(band).then(function (response) {
+        // console.log(response.data);
+        console.log("Venue: " + response.data[0].venue.name);
+        console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
+        var concertDate = moment(response.data[0].datetime).format("MM/DD/YYYY hh:00 A")
+        console.log("Date: " + concertDate);
+    })
+}
+
+function songthis() {
+    if (!type) {
+        type = "The Sign";
+    }
+
+    spotify.search({
+        type: "track",
+        query: type
+    }, function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            // console.log(data);
+            console.log("Artist: " + data.tracks.items[0].artists[0].name)
+            console.log("Track: " + data.tracks.items[0].name)
+            console.log("Album: " + data.tracks.items[0].album.name)
+            console.log("Listen: " + data.tracks.items[0].external_urls.spotify)
+        }
+    })
+};
+
+function moviethis() {
+    if (!type) {
+        type = "Mr Nobody";
+    }
+
+    var movie = "http://www.omdbapi.com/?apikey=trilogy&t=" + type;
+    axios.get(movie).then(function (response) {
+        console.log("Movie title: " + response.data.Title);
+        console.log("Release year: " + response.data.Year);
+        console.log("Imdb rating: " + response.data.Ratings[0].Value);
+        console.log("Rotten Tomatoes rating: " +response.data.Ratings[1].Value);
+        console.log("Country: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Cast: " + response.data.Actors);
+    })
+}
+
+function whatitsay(){
+    fs.readFile("random.txt", "UTF8", function (err, data) {
+        if (err) {
+            console.log(err)
+        }
+
+        var dataArr = data.split(",  ")
+        console.log(dataArr)
+        console.log(dataArr[0])
+    })
 }
